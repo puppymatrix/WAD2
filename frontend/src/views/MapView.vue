cd82ab5aa5dd0bfa49202deb4c633d50ed2cab84
Natalie <103411864+puppymatrix@users.noreply.github.com>
2023-10-15 01:10:52 +0800

    prelim map view page using leaflet

---
+++ frontend/src/views/MapView.vue
@@ -0,0 +1,53 @@
+<template>
+ <div id="container">
+   <div id="mapContainer"></div>
+ </div>
+</template>
+
+<script>
+    import "leaflet/dist/leaflet.css";
+    import L from "leaflet";
+
+    export default {
+    name: "Map",
+    data() {
+    return{
+        center: [37,7749, -122,4194]
+    }},
+
+    methods: {
+    setupLeafletMap: function () {
+        const mapDiv = L.map("mapContainer").setView(this.center, 13);
+        L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution:
+            'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            accessToken:"XXX",
+        }
+        ).addTo(mapDiv);
+    },
+    //    so that i can add the listing details later on
+    onEachFeature(feature, layer) {
+        if (feature.properties && feature.properties.name) {
+            layer.bindPopup(feature.properties.name);
+        layer.on('mouseover', () => { layer.openPopup(); });
+            layer.on('mouseout', () => { layer.closePopup(); });
+        }
+    },
+    },
+    mounted() {
+    this.setupLeafletMap();
+    },
+    };
+</script>
+
+<style scoped>
+    #mapContainer {
+    width: 80vw;
+    height: 100vh;
+    }
+</style>
+
