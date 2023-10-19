7d91ae71a24e20580b8c15eb26e25ea1d5c134b5
Natalie <103411864+puppymatrix@users.noreply.github.com>
2023-10-19 15:22:04 +0800

    layout for userProfileView

--- src/views/userProfileView.vue
+++ src/views/userProfileView.vue
@@ -2,14 +2,79 @@
 </script>
 
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 d-flex m-3">
-                <img src="../components/images/user.png"/>
-                <h1>Welcome [Name of Person]!</h1>
+<div class="container-fluid">
+    <div class="row">
+        <!-- LHS: sticky navbar -->
+        <div class="col-sm-2">
+            <div class="sideNav">
+                <a href="#userInfo" class="btn">My Info</a> 
+                <a href="#userListings" class="btn">My Listings</a> 
+                <a href="#chopedListings" class="btn">My Chopes</a> 
+                <a href="#others" class="btn">Others</a> 
             </div>
         </div>
-    </div>
+        <!-- spacer -->
+        <div class="col-1">
+
+        </div>
+        <!-- RHS actual info -->
+        <div class="col">
+            <div class="row mt-5">
+            <!-- welcome message and profile pic -->
+                <div class="col d-flex m-3" >
+                    <!-- replace with user photo -->
+                    <img id="profilePic" src="../components/images/user.png"/>
+                    <h1>Welcome {{name}}!</h1>
+                </div>
+            <!--   userInfo div -->
+            <div class="row" id="userInfo">
+                user info goes here, need to take from database later
+            </div>
+            <!-- userListings div -->
+            <div class="row" id="userListings">
+
+            </div>
+            <!-- chopedListings div -->
+            <div class="row" id="chopedListings">
+
+            </div>
+            <!-- others div -->
+            <div class="row" id="others"></div>
+        </div>
+        </div>
+        
+    </div> 
+</div>
 </template>
 
-<style></style>
+<style>
+#profilePic{
+    /* somehow need to fix width */
+}
+.sideNav{
+    position: fixed;
+    background-color: #558C03;
+    padding: 5%;
+    margin-top: 5%;
+}
+.sideNav a{
+    display: block;
+}
+.sideNav a:hover {
+  color: #f2f2f2;
+}
+</style>
+
+<script>
+
+export default {
+
+    data(){
+        return{
+            name: `User's name`,
+            userPhoto: `relativeUrl`
+        }
+        
+    }
+}
+</script>
