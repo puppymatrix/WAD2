922c060a4b997b314c689019fd970a2316d8e738
glendang23 <129526708+glendang23@users.noreply.github.com>
2023-10-19 11:25:12 +0800

    included UserProfile into the index.js

--- src/router/index.js
+++ src/router/index.js
@@ -69,6 +69,16 @@ const router = createRouter({
         component: () => import('../views/resetPassword.vue')
 
       },
+      {
+        path: '/userProfile',
+        name: 'userProfile',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../views/userProfileView.vue')
+
+      },
+
   ]
 })
 
