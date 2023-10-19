13c857bbf7e954d5debc80bf5083af1d8b4f77c2
glendang23 <129526708+glendang23@users.noreply.github.com>
2023-10-19 11:25:54 +0800

    created placeholder for user profile page

---
+++ src/views/userProfileView.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 d-flex m-3">
+                <img src="../components/images/user.png"/>
+                <h1>Welcome [Name of Person]!</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
