d358162b3ece9e755f8fec40bc4a434263e08472
Natalie <103411864+puppymatrix@users.noreply.github.com>
2023-10-22 18:27:03 +0800

    change link size and add map into navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,13 +10,15 @@ import { RouterLink, RouterView } from "vue-router";
             
         </div>
         <div class="col-6">
-            <ul class="nav justify-content-center px-4">
-
+            <ul class="nav justify-content-center">
                 <li class="nav-item">
                   <a class="nav-link" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="/allListings" >Listings</a>
+                  <a class="nav-link" href="/allListings" >Explore</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/mapView" >Map</a>
                 </li>
             </ul>
         </div>
@@ -94,7 +96,6 @@ import { RouterLink, RouterView } from "vue-router";
 
 a>li{
     color: white;
-    font-weight: bolder;
 }
 
 
@@ -104,11 +105,17 @@ div img {
 
 .nav-link{
     text-decoration: none;
-    color: #ebf1e7
+    color: #ebf1e7;
+    font-size: 1.5vw
 }
 
 .nav-link:hover{
     color: #bee5b0
+}
+
+.nav-item{
+    padding-left: 2.5%;
+    padding-right: 2.5%
 }
 
 </style>
