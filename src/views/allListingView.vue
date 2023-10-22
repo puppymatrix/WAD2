268bbb401e69ca4bf9f247e1b8d43e131df2e230
Natalie <103411864+puppymatrix@users.noreply.github.com>
2023-10-22 18:08:16 +0800

    fixed breakpoints for allListingView

--- src/views/allListingView.vue
+++ src/views/allListingView.vue
@@ -61,8 +61,8 @@ import SearchBar from "../components/SearchBar.vue";
             </div>
             <div class="album py-2">
                 <div class="container-fluid px-0">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                        <div class="col">
+                    <div class="row  g-3">
+                        <div class="col-lg-3 col-md-4 col-sm-12">
                             <div class="card shadow-sm">
                                 <img
                                     src="https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2Farchive%2Fd852987f86aeae8b65926f9e7a260c28285ea744"
@@ -93,7 +93,7 @@ import SearchBar from "../components/SearchBar.vue";
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col-lg-3 col-md-4 col-sm-12">
                             <div class="card shadow-sm">
                                 <img
                                     src="https://chipguanheng.com/wp-content/uploads/Baileys-Original-Ice-Cream-Pints-Pack-of-2-Pints-x-500ml-scaled.jpg"
@@ -124,7 +124,100 @@ import SearchBar from "../components/SearchBar.vue";
                                 </div>
                             </div>
                         </div>
-                        <div class="col">
+                        <div class="col-lg-3 col-md-4 col-sm-12">
+                            <div class="card shadow-sm">
+                                <img
+                                    src="https://chipguanheng.com/wp-content/uploads/Baileys-Original-Ice-Cream-Pints-Pack-of-2-Pints-x-500ml-scaled.jpg"
+                                    alt=""
+                                    class="card-img-top"
+                                />
+                                <div class="card-body border-top border-2">
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">Category</h6>
+                                    <h5 class="card-title">Ice cream</h5>
+                                    <p class="card-text d-flex align-items-center mb-3">
+                                        <IStreamlinetravel-map-location-pin-navigation-map-maps-pin-gps-location class="me-1"/> SMU School of Economics
+                                    </p>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary d-flex align-items-center">
+                                        <IMdiuser class="me-1"/>Glenda123
+                                    </h6>
+                                    <div
+                                        class="d-flex justify-content-between align-items-center"
+                                    >
+                                        <div class="btn-group">
+                                            <button
+                                                type="button"
+                                                class="btn btn-sm btn-outline-secondary"
+                                            >
+                                            <a href="/listing">View</a>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-12">
+                            <div class="card shadow-sm">
+                                <img
+                                    src="https://chipguanheng.com/wp-content/uploads/Baileys-Original-Ice-Cream-Pints-Pack-of-2-Pints-x-500ml-scaled.jpg"
+                                    alt=""
+                                    class="card-img-top"
+                                />
+                                <div class="card-body border-top border-2">
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">Category</h6>
+                                    <h5 class="card-title">Ice cream</h5>
+                                    <p class="card-text d-flex align-items-center mb-3">
+                                        <IStreamlinetravel-map-location-pin-navigation-map-maps-pin-gps-location class="me-1"/> SMU School of Economics
+                                    </p>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary d-flex align-items-center">
+                                        <IMdiuser class="me-1"/>Glenda123
+                                    </h6>
+                                    <div
+                                        class="d-flex justify-content-between align-items-center"
+                                    >
+                                        <div class="btn-group">
+                                            <button
+                                                type="button"
+                                                class="btn btn-sm btn-outline-secondary"
+                                            >
+                                            <a href="/listing">View</a>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-12">
+                            <div class="card shadow-sm">
+                                <img
+                                    src="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
+                                    alt=""
+                                    class="card-img-top"
+                                />
+                                <div class="card-body border-top border-2">
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">Category</h6>
+                                    <h5 class="card-title">Banana</h5>
+                                    <p class="card-text d-flex align-items-center mb-3">
+                                        <IStreamlinetravel-map-location-pin-navigation-map-maps-pin-gps-location class="me-1"/> SMU School of Economics
+                                    </p>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary d-flex align-items-center">
+                                        <IMdiuser class="me-1"/>Glenda123
+                                    </h6>
+                                    <div
+                                        class="d-flex justify-content-between align-items-center"
+                                    >
+                                        <div class="btn-group">
+                                            <button
+                                                type="button"
+                                                class="btn btn-sm btn-outline-secondary"
+                                            >
+                                            <a href="/listing">View</a>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-lg-3 col-md-4">
                             <div class="card shadow-sm">
                                 <img
                                     src="https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
