8a218f7a2b9251cc5bcbbcda8fbf7f75ed89c727
glendang23 <129526708+glendang23@users.noreply.github.com>
2023-10-24 14:00:45 +0800

    edited footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,84 +3,52 @@
 <template>
     <!--Footer-->
 
-    <div class="row footer" id="" style="height: 400px">
+    <div class="row footer">
 
         <!-- first column -->
-        <div class="col-4">
-            <div class="row">
-                <div class="col-2"></div>
-                <div class="col-8">
-                    <div class="container justify-content-center align-items-center">
-                        <div class="row">
-                            <img src="images/logo.png" alt="" class="p-4" style="width: 80%">
-                        </div>
-                        <div class="row text-white">
-                            Copyright © 2020 FoodCatch Pte Ltd. <br>
-                            All rights reserved
-                        </div>
-                        <div class="row" style="margin-top: 20%">
-                            <div class="col-10 ps-0">
-                                <img src="images/icons/instagram.png" alt="" class="img-fluid">
-                                <img src="images/icons/basketball.png" alt="" class="img-fluid">
-                                <img src="images/icons/twitter.png" alt="" class="img-fluid">
-                                <img src="images/icons/youtube.png" alt="" class="img-fluid">
-                            </div>
-                            <div class="col-2"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1"></div>
+        <div class="col-lg-4">
+            <div class="row d-flex justify-content-center align-items-center">
+                <img src=".\icons\foodcatch-logo.png" alt="" class="p-3" style="width: 250px;">
+                <p style="color: white; font-style: italic; text-align: center">Say no to food waste today!</p>
             </div>
         </div>
 
         <!-- second column -->
 
-        <div class="col-2 text-white">
+        <div class="col-lg-2 text-white p-4 text-center text-lg-start">
             <h3>Company</h3>
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link text-white" aria-current="page" href="#">About Us</a>
+                    <a class="nav-link text-white" aria-current="page" href="#aboutUs">About Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Blog</a>
+                    <a class="nav-link text-white" href="#stats">Our Success</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" aria-disabled="true">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" aria-disabled="true">Testimonials</a>
+                    <a class="nav-link text-white" href="#partners">Key Partners</a>
                 </li>
             </ul>
         </div>
 
         <!-- third column -->
-        <div class="col-2 text-white">
-            <h3>Support</h3>
+        <div class="col-lg-2 text-white p-4 text-center text-lg-start">
+            <h3>Account</h3>
 
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link text-white" aria-current="page" href="#">Help Center</a>
+                    <a class="nav-link text-white" aria-current="page" href="/logIn">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Terms of Service</a>
+                    <a class="nav-link text-white" href="/signUp">Register</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#">Legal</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" aria-disabled="true">Privacy Policy</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" aria-disabled="true">Status</a>
+                    <a class="nav-link text-white" href="/userProfile">User Profile</a>
                 </li>
             </ul>
         </div>
 
         <!-- fourth column -->
-        <div class="col-3 text-white">
+        <div class="col-lg-3 text-white p-4 text-center text-lg-start">
             <h3>Stay up to Date</h3>
             <form class="form-inline">
                 <div class="input-group"> <!-- Use Bootstrap input group to group input and button> -->
@@ -93,19 +61,37 @@
                 </div>
             </form>
         </div>
-        <div class="col-1"></div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-12 text-center copyright">
+            Copyright © 2020 FoodCatch Pte Ltd. <br>
+            All rights reserved
+        </div>
     </div>
 </template>
 
 <style>
     .footer {
-        background-color: #263238;
-        padding-top: 80px;
-        padding-bottom: 100px;
+        background-color: #0b2b26;
+        padding-top: 20px;
+        padding-bottom: 20px;
     }
 
     .footerBtn{
         background-color: #558C03;
         color: white;
+    }
+
+    .copyright{
+        background-color: #1A2327; 
+        color: white; 
+        padding: 10px;
+    }
+
+    h3{
+        color: #558C03;
+        font-weight: bold;
     }
 </style>
