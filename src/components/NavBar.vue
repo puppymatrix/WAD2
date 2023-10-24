be2fec49adad74bf077833b382f79b70b6504704
Natalie <103411864+puppymatrix@users.noreply.github.com>
2023-10-24 15:21:15 +0800

    aligned nav links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,7 +24,7 @@ import { RouterLink, RouterView } from "vue-router";
 
             <!-- navbar -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-0">
                 <li class="nav-item px-3 ml-5">
                     <a class="nav-link" href="/">Home</a>
                 </li>
