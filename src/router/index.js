0e8dad9399011f8a731c75934bb8407266e4c268
Kai Xuan <tankaixuan24@gmail.com>
2023-10-24 12:55:19 +0800

    changed to auth

--- src/router/index.js
+++ src/router/index.js
@@ -29,9 +29,6 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/listingView.vue'),
-      meta: {
-        authRequired: true,
-      },
 
     },
 
@@ -61,7 +58,10 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/addListingView.vue')
+      component: () => import('../views/addListingView.vue'),
+      meta: {
+        authRequired: true,
+      },
     },
 
     {
