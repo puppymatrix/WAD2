42fcfbe19cf772234d7b476a3dadc8ae495cc04d
glendang23 <129526708+glendang23@users.noreply.github.com>
2023-10-24 14:01:50 +0800

    combined the map view and the search bar

--- src/views/allListingView.vue
+++ src/views/allListingView.vue
@@ -4,15 +4,15 @@ import SearchBar from "../components/SearchBar.vue";
 
 <template>
     <body>
-        
-        <!-- search bar -->
-        <SearchBar />
 
         <!-- Map View button  -->
         <div class="container-fluid my-3">
             <div class="row">
-                <div class="col-10"></div>
-                <div class="col-2 d-flex justify-content-center">
+                <div class="col-10 p-0">
+                    <!-- search bar -->
+                    <SearchBar />
+                </div>
+                <div class="col-2 d-flex justify-content-center p-0">
                     <button
                         type="button"
                         class="btn btn-outline-success d-flex align-items-center justify-content-center"
