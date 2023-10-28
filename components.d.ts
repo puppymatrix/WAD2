47f4a17cd1fb0da0af84541adc7da72ffe61f1db
glendang23 <129526708+glendang23@users.noreply.github.com>
2023-10-28 17:53:25 +0800

    Update components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -7,21 +7,10 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    BCarousel: typeof import('bootstrap-vue-next')['BCarousel']
-    BCarouselSlide: typeof import('bootstrap-vue-next')['BCarouselSlide']
     BDropdown: typeof import('bootstrap-vue-next')['BDropdown']
     BDropdownItem: typeof import('bootstrap-vue-next')['BDropdownItem']
-    CarouselMarquee: typeof import('./src/components/CarouselMarquee.vue')['default']
-    IMdiuser: typeof import('~icons/mdi/user')['default']
-    IStreamlinetravelMapLocationPinNavigationMapMapsPinGpsLocation: typeof import('~icons/streamline/travel-map-location-pin-navigation-map-maps-pin-gps-location')['default']
-    Map: typeof import('./src/components/Map.vue')['default']
-    MarqueeCard: typeof import('./src/components/MarqueeCard.vue')['default']
-    Footer: typeof import('./src/components/Footer.vue')['default']
-    Map: typeof import('./src/components/Map.vue')['default']
-    MaterialInput: typeof import('./src/components/MaterialInput.vue')['default']
-    NavBar: typeof import('./src/components/NavBar.vue')['default']
+    CarouselMarquee2: typeof import('./src/components/CarouselMarquee2.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    SearchBar: typeof import('./src/components/SearchBar.vue')['default']
   }
 }
