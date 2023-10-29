4ea3289787a4f551e815dee2b20750bfdfc4f189
glendang23 <129526708+glendang23@users.noreply.github.com>
2023-10-30 00:49:01 +0800

    Update components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -7,9 +7,13 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    BCarousel: typeof import('bootstrap-vue-next')['BCarousel']
+    BCarouselSlide: typeof import('bootstrap-vue-next')['BCarouselSlide']
     BDropdown: typeof import('bootstrap-vue-next')['BDropdown']
     BDropdownItem: typeof import('bootstrap-vue-next')['BDropdownItem']
     CarouselMarquee2: typeof import('./src/components/CarouselMarquee2.vue')['default']
+    IMdiuser: typeof import('~icons/mdi/user')['default']
+    IStreamlinetravelMapLocationPinNavigationMapMapsPinGpsLocation: typeof import('~icons/streamline/travel-map-location-pin-navigation-map-maps-pin-gps-location')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
